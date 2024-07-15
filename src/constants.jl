fad48efc1f87cd7fdd0104bf3e2cf7e80b444bc4
AlparslanGuzey <66413208+AlparslanGuzey@users.noreply.github.com>
2024-07-15 23:04:26 +0300

    Create constants.jl

---
+++ src/constants.jl
@@ -0,0 +1,14 @@
+module Constants
+
+export tas, drs, drh, ddus, dbus, CUAV, CUGV, w_max
+
+tas = 200  # Toplanma Alanı Sayısı
+drs = 4  # Drone Sayısı
+drh = 10  # Drone Hızı
+ddus = 10  # Drone Dönüş Hızı
+dbus = 15  # Drone Başlangıç Hızı
+CUAV = 1  # UAV'nin tıbbi kit kapasitesi
+CUGV = 4  # UGV'nin tıbbi kit kapasitesi
+w_max = 3  # Maksimum pil değiştirme sayısı
+
+end
